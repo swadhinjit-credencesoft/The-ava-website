15ce1c57618ea8aeb8209854f3edca67b31b9c7b
swadhinjit-das <swadhinjit@credencesoft.in>
2026-06-25 20:28:34 +0530

    fixed it  hydration issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
+      </head>
       <body suppressHydrationWarning>
         <Providers>
           {children}
